547d7b1bf9b8c19d61ec4222e27e474d147a5892
Joao Alves <joao.alves@babylonhealth.com>
2017-12-06 15:46:22 +0000

    playground 1

--- app/src/main/java/com/jcmsalves/kotlinplayground/MainActivity.kt
+++ app/src/main/java/com/jcmsalves/kotlinplayground/MainActivity.kt
@@ -2,11 +2,14 @@ package com.jcmsalves.kotlinplayground
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        updateTextButton.setOnClickListener { welcomeTextView.text = "Hello Kotlin World!" }
     }
 }
