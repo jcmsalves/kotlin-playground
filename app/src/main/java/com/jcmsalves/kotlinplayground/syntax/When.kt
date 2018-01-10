acd4d3f92f46ac1324666bff6f1c886e214469a9
Joao Alves <jcmsalves@gmail.com>
2018-01-10 09:38:34 +0000

    play ground 3 - added extra when example

--- app/src/main/java/com/jcmsalves/kotlinplayground/syntax/When.kt
+++ app/src/main/java/com/jcmsalves/kotlinplayground/syntax/When.kt
@@ -10,4 +10,24 @@ class When {
             !is String -> "Not a string"
             else -> "Unknown"
         }
+
+    fun ifelseif() {
+        var number = 234
+
+        // traditional if-else-if chain
+        if (number > 0) {
+            print("number is bigger than 0")
+        } else if (number < 0) {
+            print("number is lower than 0")
+        } else {
+            print("number is 0")
+        }
+
+        // using when to replace the above chain
+        when {
+            number > 0 -> print("number is bigger than 0")
+            number < 0 -> print("number is lower than 0")
+            else -> print("number is 0")
+        }
+    }
 }
